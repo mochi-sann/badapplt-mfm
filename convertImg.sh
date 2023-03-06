e2377cb3fd6636f8803a826d38524d82cc57c286
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-03-06 21:36:28 +0900

    init

---
+++ convertImg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 画像が保存されているディレクトリのパス
+IMAGES_DIR="./files/img/"
+
+# 画像ファイルの拡張子
+IMAGE_EXT="png"
+
+OUTPUT_DIR="./files/new/"
+
+
+
+
+# 出力フォルダが存在しない場合、作成する
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir "$OUTPUT_DIR"
+fi
+
+# 画像を半分にする
+mogrify -path "$OUTPUT_DIR" -resize 10% "$IMAGES_DIR/*.$IMAGE_EXT"
---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+
+from PIL import Image
+
+
+def rgba_to_hex(r, g, b, a):
+    return "{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+def get_file_list(directory):
+    file_list = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            file_list.append(file_path)
+    return file_list
+
+
+def resize_image(image_path, new_size):
+    """
+    画像を縮小する関数
+    :param image_path: 元画像のパス
+    :param new_size: 新しいサイズ (タプル形式)
+    :return: 縮小された画像オブジェクト
+    """
+    with Image.open(image_path) as image:
+        resized_image = image.resize(new_size)
+        return resized_image
+
+
+def convertcel(file_name: str):
+    cels = []
+    img = Image.open(file_name).convert('RGBA')
+    width, height = img.size
+    imgdata = img.getdata()
+    for y in range(height):
+        y_offset = y * width
+        new_cel = []
+        for x in range(width):
+            # 画像から色データ取得
+            r, g, b, a = imgdata[y_offset+x]
+            if a > 128:  # 透明はスキップ
+
+                # セルに設定
+                # print(f'$[fg.color={rgba_to_hex(r, g,b,a)} ■]', end="")
+
+                new_cel.append(f'$[fg.color={rgba_to_hex(r, g,b,a)} ■]')
+        cels.append(new_cel)
+    return cels
+
+
+def cel_2_text(cels: list[list[str]]):
+    # new_txt = "$[position.x=-1.3,y=-2 $[scale.x=0.5,y=0.4"
+    new_txt = ""
+    for i, value in enumerate(cels):
+        add_text = " ".join(value)
+        new_txt += add_text
+        new_txt += "\n"
+
+    new_txt += ""
+    return new_txt
+
+
+def write_to_file(filename, text):
+    # 書き込み先のファイルを開く
+    with open(filename, 'w') as f:
+        # ファイルに文字列を書き込む
+        f.write(text)
+
+
+def convert_to_5digit(num):
+    num_str = str(num)
+    if len(num_str) < 5:
+        return '0' * (5 - len(num_str)) + num_str
+    else:
+        return num_str
+
+
+def main() -> None:
+    file_list = get_file_list('./files/new/')
+    file_list = sorted(file_list)
+
+    # print(file_list)
+
+    for i, value in enumerate(file_list):
+        img_cels = convertcel(value)
+
+        text = cel_2_text(img_cels)
+
+        write_to_file("./files/txt/bad_apple-" +
+                      convert_to_5digit(i) + ".txt", text)
+
+        print("write ./files/txt/bad_apple -" +
+              convert_to_5digit(i) + ".txt")
+
+    # for file in file_list:
+
+
+main()
